bbde53739081ee0fd06fd3ef7d3bdd0ff01da4c1
Oliver Bartelings <olbar23@student.sdu.dk>
2023-11-27 10:34:55 +0100

    Added class Building and Unit

---
+++ src/main/java/org/example/Building.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Building extends Unit {
+    public ArrayList sensorList;
+    public ArrayList actuatorList;
+
+    public void getSensors() {
+
+    }
+    public void getActuators(){
+
+    }
+
+}
---
+++ src/main/java/org/example/Unit.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Unit {
+    int name;
+    int id;
+    public void getId(){
+
+    }
+    public void getName(){
+
+    }
+    public void toString(){
+
+    }
+}
